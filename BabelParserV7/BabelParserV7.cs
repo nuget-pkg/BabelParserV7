076e0c922b5da39d330f758eec03cff97ec0c96b
JavaCommons Technologies <javacommons@gmail.com>
2026-01-20 13:01:39 +0900

    BabelParserV7-v2026.0120.1301.32

--- BabelParserV7/BabelParserV7.cs
+++ BabelParserV7/BabelParserV7.cs
@@ -5,9 +5,7 @@ namespace Global;
 
 public class BabelParserV7
 {
-#pragma warning disable CA1822
     public string Parse(string code, bool includeLocation = false)
-#pragma warning restore CA1822
     {
         var assembly = typeof(BabelParserV7).Assembly;
         //Log(assembly.GetManifestResourceNames());
@@ -15,29 +13,30 @@ public class BabelParserV7
         //Echo(text, "text");
         var engine = new EasyScript();
         engine.Execute(text);
-        EasyObject result = engine.EvaluateAsEasyObject(
+        engine.Execute(
             """
-                var ast = parse($1, { plugins: ["typescript"], allowReturnOutsideFunction: true });
-                // Option to turn off start/end/loc on AST @babel/parser produced
-                // https://github.com/babel/babel/issues/11239
-                const removeASTLocation = ast => {
-                   if (ast == null) return;
-                   if (Array.isArray(ast)) {
-                      ast.forEach(a => removeASTLocation(a));
-                   } else if (typeof ast === 'object') {
-                      delete ast['loc'];
-                      delete ast['start'];
-                      delete ast['end'];
-                      const values = Object.values(ast).filter(v => Array.isArray(v) || typeof v === 'object');
-                      removeASTLocation(values);
-                   }
-                };
+            // Option to turn off start/end/loc on AST @babel/parser produced
+            // https://github.com/babel/babel/issues/11239
+            const removeASTLocation = ast => {{
+               if (ast == null) return;
+               if (Array.isArray(ast)) {{
+                  ast.forEach(a => removeASTLocation(a));
+               }} else if (typeof ast === 'object') {{
+                  delete ast['loc'];
+                  delete ast['start'];
+                  delete ast['end'];
+                  const values = Object.values(ast).filter(v => Array.isArray(v) || typeof v === 'object');
+                  removeASTLocation(values);
+               }}
+            }};
+            function $$parse(code, includeLocation) {
+              var ast = parse(code, { plugins: ["typescript"], allowReturnOutsideFunction: true });
+              if (!includeLocation) removeASTLocation(ast);
+              return ast;
+            }
 
-                if (!$2) removeASTLocation(ast);
-                
-                return ast;
-                
-                """, code, includeLocation);
+            """, code, includeLocation);
+        EasyObject result = engine.CallAsEasyObject("$$parse", code, includeLocation);
         return result.ToJson(indent: true);
     }
 }
