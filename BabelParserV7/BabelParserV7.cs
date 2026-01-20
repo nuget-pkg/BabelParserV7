42fd05a87d50b5334000401bb80b45741b3b248a
JavaCommons Technologies <javacommons@gmail.com>
2026-01-20 22:54:33 +0900

    BabelParserV7-v2026.0120.2254.21

--- BabelParserV7/BabelParserV7.cs
+++ BabelParserV7/BabelParserV7.cs
@@ -9,7 +9,7 @@ public class BabelParserV7
     {
         var assembly = typeof(BabelParserV7).Assembly;
         //Log(assembly.GetManifestResourceNames());
-        var text = Sys.ResourceAsText(assembly, "BabelParserV7:https_cdn.jsdelivr.net_npm_@babel-parser@7.28.5_lib_index.js");
+        var text = Sys.ResourceAsText(assembly, "BabelParserV7:https_cdn.jsdelivr.net_npm_@babel-parser@7.28.6_lib_index.js");
         //Echo(text, "text");
         var engine = new EasyScript();
         engine.Execute(text);
@@ -17,18 +17,18 @@ public class BabelParserV7
             """
             // Option to turn off start/end/loc on AST @babel/parser produced
             // https://github.com/babel/babel/issues/11239
-            const removeASTLocation = ast => {{
+            const removeASTLocation = ast => {
                if (ast == null) return;
-               if (Array.isArray(ast)) {{
+               if (Array.isArray(ast)) {
                   ast.forEach(a => removeASTLocation(a));
-               }} else if (typeof ast === 'object') {{
+               } else if (typeof ast === 'object') {
                   delete ast['loc'];
                   delete ast['start'];
                   delete ast['end'];
                   const values = Object.values(ast).filter(v => Array.isArray(v) || typeof v === 'object');
                   removeASTLocation(values);
-               }}
-            }};
+               }
+            };
             function $$parse(code, includeLocation) {
               var ast = parse(code, { plugins: ["typescript"], allowReturnOutsideFunction: true });
               if (!includeLocation) removeASTLocation(ast);
